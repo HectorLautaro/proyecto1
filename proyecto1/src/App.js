671d0a30f9ba963b7d690bde8776b6eeac9d1664
Lautaro Lopez <lopezhector.lautaro@gmail.com>
2021-08-24 22:06:47 -0300

    actualizando

--- proyecto1/src/App.js
+++ proyecto1/src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar.js'
 
 function App(){
   return(
-       <NavBar/>
+      <NavBar/>
   )
 }
 
--- proyecto1/src/components/NavBar.js
+++ proyecto1/src/components/NavBar.js
@@ -2,17 +2,33 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from 'react-bootstrap/NavBar'
 import { Nav } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+
 function App() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-        <Navbar.Brand href="#home" className="">Cerveza Capitan!</Navbar.Brand>
-        <Nav>
-            <Nav.Link href="productos">Productos</Nav.Link>
-            <Nav.Link href="Nosotros">Nosotros</Nav.Link>
-            <Nav.Link href="Contactanos">Contactanos</Nav.Link>
-        </Nav>
-    </Navbar> 
-  )   
-}                                                                   
+          <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              </Navbar.Collapse>
+              <Nav>
 
+              </Nav>                                                                                                                           
+            </Container>
+          </Navbar>
+  )  
+}                                                                  
 export default App
